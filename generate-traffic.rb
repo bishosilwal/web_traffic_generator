cc96ac18f586688bd2e7b50f7f55c8de8627321a
Bisho silwal <aayussilwal@gmail.com>
2020-08-14 10:18:07 +0545

    config tor and run selenium

---
+++ generate-traffic.rb
@@ -0,0 +1,60 @@
+
+require 'rubygems'
+require 'byebug'
+require 'selenium-webdriver'
+require 'tormanager'
+
+threads = []
+port_pool = { 9050 => 50501, 9051 =>50502 , 9052 => 50503 }
+
+used_ports = []
+
+# threads << Thread.new do
+	# current_port = port_pool.keys.sample
+	# until !used_ports.include?(current_port) do
+	# 	current_port = port_pool.keys.sample
+	# end
+	current_port = 9050
+	# tor configuration
+	# tor_process =TorManager::TorProcess.new tor_port: current_port,
+	#                              control_port: port_pool[current_port],
+	#                              pid_dir: '/Users/silwal/files/workspace/traffic-generator/tor/pid/dir',
+	#                              log_dir: '/Users/silwal/files/workspace/traffic-generator/tor/log/dir',
+	#                              tor_data_dir: '/Users/silwal/files/workspace/traffic-generator/tor/datadir',
+	#                              tor_new_circuit_period: 120,
+	#                              max_tor_memory_usage_mb: 400,
+	#                              max_tor_cpu_percentage: 15,
+	#                              eye_logging: true,
+	#                              tor_logging: true
+
+	tor_process = TorManager::TorProcess.new
+	tor_proxy = TorManager::Proxy.new tor_process: tor_process
+	tor_ip_control = TorManager::IpAddressControl.new tor_process: tor_process, tor_proxy: tor_proxy
+	used_ports << current_port
+	# selenium configuration
+	proxy = Selenium::WebDriver::Proxy.new(socks: "127.0.0.1:#{current_port}", socks_version: 5)
+	cap   = Selenium::WebDriver::Remote::Capabilities.chrome(proxy: proxy)
+	options = Selenium::WebDriver::Chrome::Options.new
+	options.headless!
+	# generate random user agent
+	options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36")
+
+	browser = Selenium::WebDriver.for(:chrome, desired_capabilities: cap, options: options)
+	10.times do
+		tor_process.start
+
+		rand(5).times do
+			browser.get('https://mailet.in')
+			sleep rand(5)
+		end
+		tor_process.stop
+	end
+
+	wait = Selenium::WebDriver::Wait.new(timeout: 30)
+	wait.until { !browser.find_element(class: 'mail').text.empty? }
+	browser.quit
+# end
+
+# threads.each {|thr| thr.join }
+
+
