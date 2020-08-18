c4ab686ae7b937e3e3783b3dc0d16b43edf72817
Bisho silwal <aayussilwal@gmail.com>
2020-08-19 00:09:39 +0545

    basic organic request

---
+++ request_to_mailet.rb
@@ -0,0 +1,78 @@
+
+require 'rubygems'
+require 'tormanager'
+require 'byebug'
+require 'staccato'
+
+HOST = 'https://www.mailet.in'
+GOOGLE_HOST = 'https://www.google.com'
+
+USER_AGENT = [
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; Media Center PC 6.0; InfoPath.3; MS-RTC LM 8; Zune 4.7)",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; Zune 4.0; Tablet PC 2.0; InfoPath.3; .NET4.0C; .NET4.0E)",
+	"Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0;  rv:11.0) like Gecko",
+	"Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10",
+	"Mozilla/5.0 (Windows; U; Windows NT 6.1; ko-KR) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; ru-RU) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4",
+	"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-us) AppleWebKit/534.16+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4",
+	"Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+	"Opera/12.0(Windows NT 5.2;U;en)Presto/22.9.168 Version/12.00",
+	"Opera/9.80 (X11; Linux x86_64; U; bg) Presto/2.8.131 Version/11.10",
+	"Opera/9.80 (Windows NT 5.1; U; MRA 5.6 (build 03278); ru) Presto/2.6.30 Version/10.63",
+	"Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+	"Mozilla/5.0 (Linux; U; Android 2.3.5; zh-cn; HTC_IncredibleS_S710e Build/GRJ90) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+	"Mozilla/5.0 (Linux; U; Android 2.2.1; en-ca; LG-P505R Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+	"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36",
+	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36"
+]
+
+
+tor_process =TorManager::TorProcess.new tor_port: 9050,
+                             control_port: 50501,
+                             pid_dir: '/Users/silwal/files/workspace/traffic-generator/tor/pid/dir',
+                             log_dir: '/Users/silwal/files/workspace/traffic-generator/tor/log/dir',
+                             tor_data_dir: '/Users/silwal/files/workspace/traffic-generator/tor/datadir',
+                             tor_new_circuit_period: 15,
+                             max_tor_memory_usage_mb: 400,
+                             max_tor_cpu_percentage: 15,
+                             eye_logging: true,
+                             tor_logging: true
+tor_proxy = TorManager::Proxy.new tor_process: tor_process
+tor_ip_control = TorManager::IpAddressControl.new tor_process: tor_process, tor_proxy: tor_proxy
+tor_process.start
+
+tor_proxy = TorManager::Proxy.new tor_process: tor_process
+
+pageview_hash = {
+	path: '/',
+	hostname: HOST,
+	title: 'Temporary Disposable Email generator',
+	user_agent: USER_AGENT.sample,
+	referrer: GOOGLE_HOST,
+	data_source: 'web',
+	campaign_source: 'google',
+	campaign_medium: 'organic',
+	campaign_keyword: 'fake email id generator'
+}
+
+tor_proxy.proxy  do
+	50.times do |i|
+		tracker = Staccato.tracker('UA-173576841-1', nil, ssl: true)
+		puts 'requesting...'
+		tracker.pageview(pageview_hash)
+		if(i % 5 == 0)
+			begin
+				tor_ip_control.get_new_ip
+			rescue
+			end
+		end
+	end
+end
+
+tor_process.stop
+system('eye q -s')
